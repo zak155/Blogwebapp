83bdeb6d8f83d4ef0b4d2c1dedda65116ec8fbd0
zak155 <zekaryasgebeyaw48@gmail.com>
2026-06-05 14:35:42 +0300

    post by category completed

--- blogs/urls.py
+++ blogs/urls.py
@@ -4,5 +4,5 @@ from django.conf import settings
 
 from blogs import views
 urlpatterns=[
-    path('<int:category_id>/',views.post_by_category,name='post_bycategory'),
+    path('<int:category_id>/',views.post_by_category,name='post_by_category'),
 ]

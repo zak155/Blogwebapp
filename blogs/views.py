34ed858f091d825ecb2e37d11c48a285946614e3
zak155 <zekaryasgebeyaw48@gmail.com>
2026-06-15 12:35:12 +0300

    feat:adding continue reading feature

--- blogs/views.py
+++ blogs/views.py
@@ -12,6 +12,13 @@ def post_by_category(request,category_id):
     category=get_object_or_404(Category,pk=category_id)    
     context={
         'posts':posts,
-        'category':category
+        'category':category,
     }
     return render(request,'posts_by_category.html',context)   
+
+def blogs(request,slug):
+    single_blog=get_object_or_404(Blog,slug=slug,status='Published')
+    context={
+        'single_blog':single_blog
+    }
+    return render(request,'blogs.html',context)
--- config/urls.py
+++ config/urls.py
@@ -20,9 +20,11 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from blogs import views as BlogViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('category/',include('blogs.urls')),
+    path('<slug:slug>/',BlogViews.blogs,name='blogs'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

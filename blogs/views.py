711098b35e52db2e5e36e1f55fd1aa4c57aecbfa
zak155 <zekaryasgebeyaw48@gmail.com>
2026-06-16 15:44:59 +0300

    feat:search with Q object completed

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import redirect, render,get_object_or_404
 
 from blogs.models import Blog,Category
+from django.db.models import Q
 
 # Create your views here.
 def post_by_category(request,category_id):
@@ -22,3 +23,10 @@ def blogs(request,slug):
         'single_blog':single_blog
     }
     return render(request,'blogs.html',context)
+def search(request):
+    keyword=request.GET.get('keyword')
+    blogse=Blog.objects.filter(Q(title__icontains=keyword) | Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword),status='Published')
+    context={
+        'blogs':blogse
+    }
+    return render(request,'search.html',context)
--- config/urls.py
+++ config/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/',include('blogs.urls')),
     path('<slug:slug>/',BlogViews.blogs,name='blogs'),
+    path('blogs/search',BlogViews.search,name='search')
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)

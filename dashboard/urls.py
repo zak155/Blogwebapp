b2071825aef38e20f239ca914e771e008e2a8562
zak155 <zekaryasgebeyaw48@gmail.com>
2026-06-20 22:40:46 +0300

    post crud operation completed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('categories/delete-category/<int:id>',views.delete_category,name='delete_category'),
     path('posts',views.posts,name='posts'),
     path('posts/add-post',views.add_post,name='add_post'),
-    
+    path('posts/edit-post/<int:id>',views.edit_post,name='edit_post'),
+    path('posts/delete-post/<int:id>',views.delete_post,name='delete_post'),
     ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 
 from blogs.models import Blog, Category
 from .forms import BlogPostForm, CategoryForm
+from django.template.defaultfilters import slugify
 
 login_required(login_url='login')
 def dashboard(request):
@@ -56,9 +57,30 @@ def add_post(request):
             post=form.save(commit=False)
             post.author=request.user
             post.save()
+            title=form.cleaned_data['title']
+            post.slug=slugify(title) + '-' +str(post.id)
+            post.save()
             return redirect('posts')
         
     else:
         form=BlogPostForm()
     return render(request,'dashboard/add_post.html', {'form': form})
-   
+
+def edit_post(request,id):
+    post=get_object_or_404(Blog, id=id)
+    if request.method=='POST':
+        form=BlogPostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save(commit=False)
+            title=form.cleaned_data['title']
+            post.slug=slugify(title) + '-' +str(post.id)
+            return redirect('posts')
+    else:
+        form=BlogPostForm(instance=form)
+    return render(request,'dashboard/edit_post.html', {'form': form})
+
+def delete_post(request,id):
+    post=get_object_or_404(Blog, id=id)
+    if request.method=='POST':
+        post.delete()
+        return redirect('posts')    
